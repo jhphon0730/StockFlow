76b1087b06cdad17d76c7e821108437f79b89eba
naithar01 <eeung025@gmail.com>
2025-03-22 19:01:31 +0900

    feat : 재고내역 조회 Service 추가

--- internal/services/transaction_service.go
+++ internal/services/transaction_service.go
@@ -1,9 +1,14 @@
 package services
 
-import "github.com/jhphon0730/StockFlow/internal/repositories"
+import (
+	"github.com/jhphon0730/StockFlow/internal/models"
+	"github.com/jhphon0730/StockFlow/internal/repositories"
+
+	"net/http"
+)
 
 type TransactionService interface {
-
+	FindAll() (int, []models.Transaction, error)
 }
 
 type transactionService struct {
@@ -14,4 +19,13 @@ func NewTransactionService(transactionRepository repositories.TransactionReposit
 	return &transactionService{
 		transactionRepository: transactionRepository,
 	}
+}
+
+func (t *transactionService) FindAll() (int, []models.Transaction, error) {
+	transactions, err := t.transactionRepository.FindAll()
+	if err != nil {
+		return http.StatusInternalServerError, nil, err
+	}
+
+	return http.StatusOK, transactions, nil
 }
