80ff0b095da52d0020c04a736b10397e34c6a897
naithar01 <eeung025@gmail.com>
2025-03-22 13:38:12 +0900

    feat : 재고내역 모두 조회 및 단일 조회 기능 개발  - 단일 조회 시에는 재고를 같이 조회할 수 있는 기능을 추가

--- internal/repositories/transaction_repository.go
+++ internal/repositories/transaction_repository.go
@@ -1,8 +1,14 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/jhphon0730/StockFlow/internal/models"
+
+	"gorm.io/gorm"
+)
 
 type TransactionRepository interface {
+	FindAll() ([]models.Transaction, error)
+	FindByID(id uint) (*models.Transaction, error)
 }
 
 type transactionRepository struct {
@@ -14,3 +20,29 @@ func NewTransactionRepository(db *gorm.DB) TransactionRepository {
 		db: db,
 	}
 }
+
+// 모든 재고내역 조회 
+func (r *transactionRepository) FindAll() ([]models.Transaction, error) {
+	var transactions []models.Transaction
+
+	if err := r.db.Find(&transactions).Error; err != nil {
+		return nil, err
+	}
+
+	return transactions, nil
+}
+
+// 재고내역 조회
+func (r *transactionRepository) FindByID(id uint) (*models.Transaction, error) {
+	var transaction models.Transaction
+
+	if err := r.db.Preload("Inventory").First(&transaction, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &transaction, nil
+}
+
+// 재고내역 생성
+
+// 재고내역 삭제
