3381b5559c875087ce41674027bdc18b803dd14b
jeongho0730 <eeung025@gmail.com>
2025-03-24 09:25:02 +0900

    feat : 재고내역 핸들러에 모든 재고내역 조회 기능 추가

--- internal/handlers/transaction_handler.go
+++ internal/handlers/transaction_handler.go
@@ -1,16 +1,35 @@
 package handlers
 
-import "github.com/jhphon0730/StockFlow/internal/repositories"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jhphon0730/StockFlow/internal/services"
+	"github.com/jhphon0730/StockFlow/pkg/utils"
+)
 
 type TransactionHandler interface {
+	GetAllTransaction(c *gin.Context)
 }
 
 type transactionHandler struct {
-	transactionRepository repositories.TransactionRepository
+	transactionService services.TransactionService
 }
 
-func NewTransactionHandler(transactionRepository repositories.TransactionRepository) TransactionHandler {
+func NewTransactionHandler(transactionService services.TransactionService) TransactionHandler {
 	return &transactionHandler{
-		transactionRepository: transactionRepository,
+		transactionService: transactionService,
 	}
+}
+
+func (t *transactionHandler) GetAllTransaction(c *gin.Context) {
+	status, transactions, err := t.transactionService.FindAll()
+	if err != nil {
+		utils.JSONResponse(c, status, nil, err)
+		return
+	}
+
+	res_data := gin.H{
+		"transactions": transactions,
+	}
+
+	utils.JSONResponse(c, status, res_data, nil)
 }
