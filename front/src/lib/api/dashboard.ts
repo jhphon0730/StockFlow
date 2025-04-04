65b462caaf56d512217467e3fc46b62593b2832d
jhphon0730 <eeung025@gmail.com>
2025-04-04 15:39:50 +0900

    feat : 대시보드 데이터 구성 백엔드 연결 추가

--- front/src/lib/api/dashboard.ts
+++ front/src/lib/api/dashboard.ts
@@ -1,8 +1,18 @@
 import { FetchWithAuth, Response } from "@/lib/api";
-import { WebSocketInformation } from "@/types/dashboard";
+import { WebSocketInformation, DashboardInfo } from "@/types/dashboard";
 
 export const GetRoomInfo = async (): Promise<Response<WebSocketInformation[]>> => {
 	const res = await FetchWithAuth('/ws/room', {
+		method: "GET",
+	})
+	return {
+		data: res.data,
+		error: res.error,
+	}
+}
+
+export const GetDashboardInfo = async (): Promise<Response<DashboardInfo>> => {
+	const res = await FetchWithAuth('/dashboard', {
 		method: "GET",
 	})
 	return {
--- front/src/types/dashboard/index.ts
+++ front/src/types/dashboard/index.ts
@@ -1,28 +1,18 @@
+import { Transaction } from "@/types/transaction";
+
 export interface WebSocketInformation {
-	roomID: string;
-	connectedClientCount: number;
+  roomID: string;
+  connectedClientCount: number;
 }
 
-// 대시보드에 구성되는 데이터의 반환 값
+export interface CountComparison {
+  count: number;
+  comparison: number;
+}
+
 export interface DashboardInfo {
-
+  inventory: CountComparison;
+  product: CountComparison;
+  warehouse: CountComparison;
+  recent_transactions: Transaction[];
 }
-/*
-{
-    "data": {
-        "inventory": {
-            "comparison": 100,
-            "count": 2
-        },
-        "product": {
-            "comparison": 80,
-            "count": 9
-        },
-        "recent_transactions": [],
-        "warehouse": {
-            "comparison": 0,
-            "count": 2
-        }
-    }
-}
-*/
