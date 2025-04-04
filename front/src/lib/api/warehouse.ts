7ac521fad4ecaeb54366193ad2d94a7baa66743c
jhphon0730 <eeung025@gmail.com>
2025-04-04 16:37:31 +0900

    log : 앱 로그 삭제

--- front/src/lib/api/warehouse.ts
+++ front/src/lib/api/warehouse.ts
@@ -5,7 +5,6 @@ export const GetAllWarehouses = async (): Promise<Response<WarehouseResponse>> =
 	const res = await FetchWithAuth('/warehouses', {
 		method: "GET",
 	})
-	console.log(res.data)
 	return {
 		data: res.data,
 		error: res.error,
